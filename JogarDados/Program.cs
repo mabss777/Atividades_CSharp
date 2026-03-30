bbc66701ddf8fc7340c9931303c73d7e854e846d
Souza Amabile (SO/ETS-BR) <soa8jvl@bosch.com>
2026-03-30 11:51:05 -0300

    Atividade Jogar dados - Não finalizada

--- JogarDados/Program.cs
+++ JogarDados/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+        Console.WriteLine("Sejam bem vindos ao jogo!");
+
+        Console.WriteLine("Digite o nome do 1° jogador: ");
+        string Jogador1 = (Console.ReadLine());
+
+        Console.WriteLine("Digite o nome do 2° jogador: ");
+        string Jogador2 = (Console.ReadLine());
+
+        Random numeroDado = new Random();
+
+        for(int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"{i+1}° JOGADA!");
+        }
+
+public class Jogador
+{
+   public string Nome;
+   public int pontuacao;
+   
+
+} 
